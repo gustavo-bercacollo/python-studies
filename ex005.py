8cd8c82a226e0c86e63e91c350650f765e3cb30c
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-25 02:31:02 -0300

    exercise 005

---
+++ ex005.py
@@ -0,0 +1,3 @@
+enter_user = int(input('Enter with a number: '))
+
+print('Analyzing the number {}, your predecessor is {} and your successor is {}'.format(enter_user, (enter_user - 1), (enter_user + 1)))
