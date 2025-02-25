76a069df49f1619dba0479c4660a75dbe30abec2
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-25 13:36:17 -0300

    exercise 007

---
+++ ex007.py
@@ -0,0 +1,13 @@
+# Python Exercise #007 - Arithmetic Mean
+
+first_grade = int(input("student's first grade: "))
+second_grade = int(input("student's second grade: "))
+
+print('The mean of {:.1f} and {:.1f} is {:.1f}'.format(first_grade, second_grade, ((first_grade + second_grade) / 2)))
+
+
+
+
+
+
+
