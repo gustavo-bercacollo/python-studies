aea7e943a21f701540f51733199b30a6ce3ea919
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-02-27 15:29:23 -0300

    exercise 024

---
+++ ex024.py
@@ -0,0 +1,8 @@
+# Exercise Python #024 - Checking the First Words of a Text
+
+user_enter = str(input('What city were you born in? ')).strip().lower()
+
+split_phase = user_enter.split()[0]
+check_work = 'santo' in split_phase
+
+print(check_work)
