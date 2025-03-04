0299a1d1e64cd5b13792d080c079fc1c480474ed
Gustavo Berçacollo Vilela <gustavovilela802@gmail.com>
2025-03-04 17:02:36 -0300

    exercise 030

---
+++ ex030.py
@@ -0,0 +1,9 @@
+# Python Exercise #030 - Even or Odd?
+
+num = int(input('\033[1;35mEnter with a number to know if is even or odd:\033[m '))
+
+if num % 2 == 1:
+    print(f'The number {num} is \033[1;34mEVEN\033[m')
+else:
+    print(f'The number {num} is \033[1;34mODD\033[m')
+
